5bc53802fa77eba2d9839b306892281e8e6014b0
Aryantt27 <kukeluarga545@gmail.com>
2024-05-30 17:11:33 +0700

    praktikum8

---
+++ vue-project-praktikum8/src/components/ArtikelHome.vue
@@ -0,0 +1,13 @@
+<template>
+    <header>
+    <img src="../assets/ucok.jpeg" alt="" style="height: 300px;">
+    </header>
+    <div>
+       
+        <h1>Ini adalah komponen artikel home</h1>
+        <p>Ini adalah komponen artikel home</p>
+        <p>Ini adalah komponen artikel home</p>
+        <p>Ini adalah komponen artikel home</p>
+        <p>Ini adalah komponen artikel home</p>
+    </div>
+</template>
---
+++ vue-project-praktikum8/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import ArtikelHome from '../components/ArtikelHome.vue'
+
+</script>
+
+<template>
+  <main>
+  <h1>Custom Halaman</h1>
+  <ArtikelHome />
+ <!-- <TheWelcome/>  -->
+  </main>
+</template>
